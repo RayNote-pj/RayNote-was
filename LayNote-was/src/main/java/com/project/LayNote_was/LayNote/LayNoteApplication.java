330f0a0fbca29efcb13fde30f567908383075194
als5855 <wlals5855@naver.com>
2025-08-04 00:55:08 +0900

    was start

---
+++ LayNote-was/src/main/java/com/project/LayNote_was/LayNote/LayNoteApplication.java
@@ -0,0 +1,13 @@
+package com.project.LayNote_was.LayNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayNoteApplication.class, args);
+	}
+
+}
---
+++ LayNote-was/src/test/java/com/project/LayNote_was/LayNote/LayNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.LayNote_was.LayNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
