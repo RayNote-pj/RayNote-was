839da5424a82979717132d394e57861dacc8b736
als5855 <wlals5855@naver.com>
2025-08-12 15:58:14 +0900

    create: note_project_join

---
+++ lay_note_was/src/main/java/com/project/lay_note_was/lay_note/dto/note_project_join/response/NoteProjectJoinResponseDto.java
@@ -0,0 +1,13 @@
+package com.project.lay_note_was.lay_note.dto.note_project_join.response;
+
+import com.project.lay_note_was.lay_note.dto.note_project_join.NoteProjectJoinDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NoteProjectJoinResponseDto {
+    private NoteProjectJoinDto noteProjectJoinDto;
+}
--- lay_note_was/src/main/java/com/project/lay_note_was/lay_note/service/NoteProjectJoinService.java
+++ lay_note_was/src/main/java/com/project/lay_note_was/lay_note/service/NoteProjectJoinService.java
@@ -1,4 +1,15 @@
 package com.project.lay_note_was.lay_note.service;
 
+import com.project.lay_note_was.lay_note.dto.ResponseDto;
+import com.project.lay_note_was.lay_note.dto.note_project_join.request.NoteProjectJoinRequestDto;
+import com.project.lay_note_was.lay_note.dto.note_project_join.response.NoteProjectJoinResponseDto;
+import com.project.lay_note_was.lay_note.entity.note_project_join.JoinStatus;
+
 public interface NoteProjectJoinService {
+
+    ResponseDto<NoteProjectJoinResponseDto> createJoin(String userEmail, NoteProjectJoinRequestDto dto);
+
+    ResponseDto<NoteProjectJoinResponseDto> changeJoinStatus(String userEmail, String noteProjectJoinId, JoinStatus joinStatus);
+
+    ResponseDto<NoteProjectJoinResponseDto> getJoin(String userEmail, String noteProjectJoinId);
 }
